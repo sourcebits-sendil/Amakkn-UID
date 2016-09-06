6d6f29bf1eb455082663f00e14fc2c4d197d8c13
devansh <devansh.kumar@sourcebits.com>
2016-09-06 23:50:30 +0530

    property description.

--- scripts/Application/Controllers/propertyDetails.controller.js
+++ scripts/Application/Controllers/propertyDetails.controller.js
@@ -14,6 +14,7 @@
     function propertyDetailsController ($log, $scope, $http, $timeout, $filter, httpService, $location, $rootScope, $mdToast, NgMap) {
         var vm = this;
         $scope.imageArray = null;
+        $scope.diffDays = null;
         vm.class = 'propertyDetailsController';
         $scope.propertiesData = null;
         activate();
@@ -32,19 +33,17 @@
                 $scope.propertiesData = result.response;
                 var images = result.response.photos;
                 $scope.imageArray = images.split(',');
+                var date1 = new Date(result.response.createdAt);
+				var date2 = new Date();
+				var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+				$scope.diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
             });
         }
         $scope.propertyList();
-        $scope.getImage = function(photos)
+        /*$scope.getImage = function(photos)
         {
             var photosArr = photos.split(',');
             return photosArr[0];
-        }
-        $scope.getPropertyType = function(id)
-        {
-        	$scope.urlRest = 'http://52.42.99.192/Property/getPropertyDescription/';
-            var photosArr = photos.split(',');
-            return photosArr[0];
-        }
+        }*/
     }
 })();
