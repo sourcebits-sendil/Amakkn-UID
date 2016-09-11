aee1ed8b25a396b03f9c3d491ec4af2a3be7ae26
devansh <devansh.kumar@sourcebits.com>
2016-09-11 17:00:06 +0530

    added myFavourites Controller

---
+++ scripts/Application/Controllers/myFavourites.controller.js
@@ -0,0 +1,48 @@
+/**
+ * @author: Devansh
+ * @since(mm/dd/yy): 9/11/2016
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('amakkn')
+        .controller('myFavouritesController', myFavouritesController);
+
+    /* @ngInject */
+    function myFavouritesController ($log, $scope, $http, $timeout, $filter, httpService, $location, $rootScope, NgMap, $mdToast) {
+        var vm = this;
+        vm.class = 'myFavouritesController';
+        $scope.propertiesData = null;
+
+        activate();
+
+        //////////////
+
+        function activate() {
+            //$log.debug('Activating ' + vm.class);
+        }
+        $scope.myFavouritiesList = function(){
+            $scope.urlRest = 'http://52.42.99.192/Property/getFavouritesForUser/';
+            var param = {"userId":"1","page":"1"};
+            $rootScope.myPromise = httpService.getData($scope.urlRest, param).then(function(result) {    
+                if(result.resCode == 0){
+                    debugger;
+                    $scope.propertiesData = result.response.propertyArray;
+                    $rootScope.propertyArr = result.response.propertyArray;
+                    $scope.propertiesCount = result.response.totalCount;
+                }
+
+            });
+        }
+        $scope.myFavouritiesList();
+        $scope.getImage = function(photos)
+        {
+            if (photos != "") 
+            {
+                var photosArr = photos.split(',');
+                return photosArr[0];
+            }
+        }
+    }
+})();
