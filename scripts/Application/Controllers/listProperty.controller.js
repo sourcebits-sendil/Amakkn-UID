0aaaa21e3da8b9154ace46610434bd3c794232af
jyothichethan1 <jyothi.byraiah@sourcebits.com>
2016-09-23 16:05:07 +0530

    changes

--- scripts/Application/Controllers/listProperty.controller.js
+++ scripts/Application/Controllers/listProperty.controller.js
@@ -102,6 +102,38 @@
     dimension: " $",
     scale: [1960, '|', 1970, '|', 1980, '|' , 1990, '|', 2000, '|', 2015]
   };
+        
+        
+        //pagignation
+       /* $scope.showData = function( ){
+
+ $scope.curPage = 0;
+ $scope.pageSize = 3;
+     $scope.datalists = [
+         { "name": "John","age":"16","designation":"Software Engineer1"},
+    {"name": "John2","age":"21","designation":"Software Engineer2"},
+    {"name": "John3","age":"19","designation":"Software Engineer3"},
+    {"name": "John4","age":"17","designation":"Software Engineer4"},
+    {"name": "John5","age":"21","designation":"Software Engineer5"},
+    {"name": "John6","age":"31","designation":"Software Engineer6"},
+    {"name": "John7","age":"41","designation":"Software Engineer7"},
+    {"name": "John8","age":"16","designation":"Software Engineer8"},
+    {"name": "John18","age":"16","designation":"Software Engineer9"},
+    {"name": "John28","age":"16","designation":"Software Engineer10"},
+    {"name": "John38","age":"16","designation":"Software Engineer11"},
+    {"name": "John48","age":"16","designation":"Software Engineer12"},
+    {"name": "John58","age":"16","designation":"Software Engineer13"},
+    {"name": "John68","age":"16","designation":"Software Engineer14"},
+    {"name": "John68","age":"16","designation":"Software Engineer15"}
+]
+     $scope.numberOfPages = function() {
+				return Math.ceil($scope.datalists.length / $scope.pageSize);
+			};
+         
+};
+      */
+
+
 
         $scope.amenityList = function(){
             $scope.urlRest = 'http://52.42.99.192/Property/getAmenitiesForPropertyType/';
